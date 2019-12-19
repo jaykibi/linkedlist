1ce9ff8a8b298db100b7d9c0b4ecf89f7300b71e
raquibbi <raquibbi@gmail.com>
2019-12-19 03:58:31 +0000

    cleaned up linkedlist2 code

--- linkedlist2.rb
+++ linkedlist2.rb
@@ -5,17 +5,19 @@ class LinkedListNode
     @value = value
     @next_node = next_node
   end
-end
+end 
 
 def print_values(list_node)
-  print "#{list_node.value} --> "
-  if list_node.next_node.nil?
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
     print "nil\n"
     return
-  else
-    print_values(list_node.next_node)
   end
+
 end
+
 def reverse_list(list, previous=nil)
   current_head = list.next_node
   list.next_node = previous
@@ -29,7 +31,11 @@ end
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
+
 print_values(node3)
+
 puts "-------"
+
 revlist = reverse_list(node3)
+
 print_values(revlist)
